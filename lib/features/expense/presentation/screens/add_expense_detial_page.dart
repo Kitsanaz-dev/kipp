44db0e1c9c10bab18e656d0c5b08a4e0f8ff83d8
Kitsanaz-Dev <kitsanaz.ctbd@gmail.com>
2026-08-25 15:11:08 +0700

    Add floating action button to AddExpenseDetailPage

--- lib/features/expense/presentation/screens/add_expense_detial_page.dart
+++ lib/features/expense/presentation/screens/add_expense_detial_page.dart
@@ -1,8 +1,12 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:kipp/core/constant/radius.dart';
+import 'package:kipp/core/router/route_paths.dart';
 import 'package:kipp/core/theme/app_theme.dart';
 import 'package:kipp/features/expense/domain/entities/expense_entity.dart';
 import 'package:kipp/features/expense/presentation/providers/expense_provider.dart';
@@ -294,6 +298,14 @@ class _AddExpenseDetailPageState extends ConsumerState<AddExpenseDetailPage> {
             ],
           ),
         ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: context.colors.primary,
+        child: Icon(Icons.camera_alt_outlined, color: context.colors.onPrimary),
+        onPressed: () {
+
+        },
       ),
     );
   }
